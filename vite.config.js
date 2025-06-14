f93ad1af53869094b938277f1978252841633aa6
Jomar Júnior de Souza Pereira <jomar.junior@cern.ch>
2025-06-13 22:52:52 -0300

    feat: Update configuration and add gh-pages for deployment

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
+  base: '/our-love-lab',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
